62a36c284aa992ffac340bdea39d6c9d757f412e
Ganesh Mohan <ganeshmohan99@gmail.com>
2017-10-05 17:42:38 +0530

    changing filename by replacing space with underscore

--- app/assets/javascripts/jquery-fileupload/jquery.fileupload.js
+++ app/assets/javascripts/jquery-fileupload/jquery.fileupload.js
@@ -452,7 +452,6 @@
                 options.headers['Content-Range'] = options.contentRange;
             }
             if (!multipart || options.blob || !this._isInstanceOf('File', file)) {
-                alert(3);
                 options.headers['Content-Disposition'] = 'attachment; filename="' +
                     encodeURI(file.name) + '"';
             }
@@ -490,7 +489,7 @@
                         });
                     }
                     if (options.blob) {
-                        formData.append(paramName, options.blob, file.name);
+                        formData.append(paramName, options.blob, file.name.replace(/ /g, "_"));
                     } else {
                         $.each(options.files, function (index, file) {
                             // This check allows the tests to run with
@@ -501,7 +500,7 @@
                                     ($.type(options.paramName) === 'array' &&
                                         options.paramName[index]) || paramName,
                                     file,
-                                    "aaaaaa.jpg"
+                                    file.uploadName.replace(/ /g, "_") || file.name.replace(/ /g, "_")
                                 );
                             }
                         });
@@ -1191,11 +1190,9 @@
                 // If the files property is not available, the browser does not
                 // support the File API and we add a pseudo File object with
                 // the input value as name with path information removed:
-                alert(1);
                 files = [{name: value.replace(/^.*\\/, '')}];
             } else if (files[0].name === undefined && files[0].fileName) {
                 // File normalization for Safari 4 and Firefox 3:
-                alert(2);
                 $.each(files, function (index, file) {
                     file.name = file.fileName;
                     file.size = file.fileSize;
